7379a37f93f1c852e5f95dea03740adea8f9f917
RafM <rkjmosiolek@gmail.com>
2017-07-13 17:18:49 +0100

    add google maps api logic

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('\'Allo \'Allo!');
+console.log("Hello!");
+
+
+var searchBox = new google.maps.places.SearchBox(document.querySelector("#city-search"));
+searchBox.addListener("places_changed", function() {
+    var locale = searchBox.getplaces()[0];
+
+    var latInput = document.querySelector("#latitude");
+    var lngInput = document.querySelector("#longitude");
+
+    latInput.value = place.geometry.location.lat();
+    lngInput.value = place.geometry.location.lng();
+});
+
