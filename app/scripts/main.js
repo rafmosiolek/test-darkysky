2c647224f7aeaf4d9e7108762e483c6f1ac76456
RafM <rkjmosiolek@gmail.com>
2017-07-14 16:39:01 +0100

    use navigator.geolocation API to locate user's device

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,56 +1,60 @@
-console.log("Hello!");
 
-// store reference to #city-search input in a citySearch variable
-var citySearch = document.getElementById("#city-search");
-// attach citySearch to a Google Maps API SearchBox constructor
-var searchBox = new google.maps.places.SearchBox(citySearch);
-searchBox.addListener("places_changed", function() {
-	var locale = searchBox.getplaces()[0];
+// 	function geoLocation() {
+// 		var userLocation = document.getElementById("user-location");
 
-	var latInput = document.querySelector("#latitude");
-	var lngInput = document.querySelector("#longitude");
+// 		if (!navigator.geolocation) {
+// 			userLocation.innerHTML = "<h2>Geolocation is not supported by your browser!</h2>";
+// 			return;
+// 		}
 
-	latInput.value = place.geometry.location.lat();
-	lngInput.value = place.geometry.location.lng();
-});
+// 		function success(position) {
+// 			var latitude = position.coords.latitude;
+// 			var longitude = position.coords.longitude;
 
+// 			console.log(latitude);
+// 			console.log(longitude);
 
+// 			userLocation.innerHTML = "<h1>Your location is: " + latitude + "° ," + longitude + "°</h1>";
+// 		}
 
-// pull in data from Darksky API 
-$.getJSON('https://api.darksky.net/forecast/c78e6b910ba14ef6ac41cfc1e46b3af5/51.5074,0.1278', function(forecast) {
-	console.log(forecast);
-});
+// 		function error() {
+// 			userLocation.innerHTML = "<h2>Unable to retrieve your location!</h2>";
+// 		}
 
+// 		navigator.geolocation.getCurrentPosition(success, error);
+// 	}
 
+// 	var body = document.querySelector("body");
+// 	body.addEventListener("onload", geoLocation);
 
+// };
 
+window.onload = function() {
+	console.log("page loaded");
+	getLocation();
+	
+	var userLocation = document.getElementById("userLocation");
+	console.log(userLocation);
+	
+	function getLocation() {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(showPosition);
 
-function weather() {
-
-	var location = document.getElementById("city-search");
-	var apiKey = 'c78e6b910ba14ef6ac41cfc1e46b3af5'; 
-	var url = 'https://api.darksky.net/forecast/';
-
-	navigator.geolocation.getCurrentPosition(success, error);
-
-	function success(position) {
-		latitude = position.coords.latitude;
-		longitude = position.coords.longitude;
-
-		location.innerHTML = 'Latitude is ' + latitude + '° Longitude is ' + longitude + '°';
-
-		$.getJSON(url + apiKey + "/" + latitude + "," + longitude + "?callback=?", function(data) {
-			$('#temp').html(data.currently.temperature + '° F');
-			$('#minutely').html(data.minutely.summary);
-			console.log(data);
-		});
+		} else {
+			userLocation.innerHTML = "<h2>Geolocation is not supported by this browser.</h2>";
+		}
 	}
 
-	function error() {
-		location.innerHTML = "Unable to retrieve your location";
+	function showPosition(position) {
+		
+
+		var latitude = position.coords.latitude;
+		console.log(latitude);
+		var longitude = position.coords.longitude;
+		console.log(longitude);
+		userLocation.innerHTML = "<h1>Your location is: " + latitude + "° ," + longitude + "°</h1>";
 	}
 
-	location.innerHTML = "Locating...";
-}
 
-weather();
+
+};
